18f031aa62649df8fe5f674d9e8c77dbdd7d1979
Olga Rosas <olga.rosas@gmail.com>
2019-05-25 15:32:02 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,28 +1,27 @@
 def division(num1, num2)
- num1 / num 2
+ num1 / num2
 end
 
 def assign_variable(value)
-  name = value
+  
 end
 
 def argue
-  return argue
+  
 end
 
-def greeting(greeting, name)
-  puts "#{greeting}, #{name}!"
+def greeting
+
 end
 
 def return_a_value
-  return "Nice"
+
 end
 
 def last_evaluated_value
-  return "expert"
+
 end
 
-def pizza_party(topping = "cheese")
-  return topping
+def pizza_party
 
 end
